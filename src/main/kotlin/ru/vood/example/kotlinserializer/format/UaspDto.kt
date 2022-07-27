da80ccfdfd3369dfd6535f5c1dcbb34c675a3496
vood <balitsky_igor@list.ru>
2022-07-27 17:09:22 +0300

    s

--- src/main/kotlin/ru/vood/example/kotlinserializer/format/UaspDto.kt
+++ src/main/kotlin/ru/vood/example/kotlinserializer/format/UaspDto.kt
@@ -1,11 +1,15 @@
 package ru.vood.example.kotlinserializer.format
 
 import kotlinx.serialization.Serializable
+import java.math.BigDecimal
 
 @Serializable
 data class UaspDto(
     val id: String,
     val dataString: Map<String, String>,
-//                   val dataDecimal: Map<String, BigDecimal>,
-    val dataDecimal: Map<String, Double>,
-)
+    val dataDouble: Map<String, Double>,
+    @Serializable(with = BigDecimalSerializer::class)
+    val dataDecimal: BigDecimal,
+
+    )
+

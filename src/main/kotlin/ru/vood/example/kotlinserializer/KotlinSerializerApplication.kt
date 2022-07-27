c7db146ccd72c68bc516f00d1b258131913e7da1
IBalitskiy <IBalitskiy@inno.tech>
2022-07-27 10:24:17 +0300

    sd

---
+++ src/main/kotlin/ru/vood/example/kotlinserializer/KotlinSerializerApplication.kt
@@ -0,0 +1,11 @@
+package ru.vood.example.kotlinserializer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSerializerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSerializerApplication>(*args)
+}
---
+++ src/test/kotlin/ru/vood/example/kotlinserializer/KotlinSerializerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vood.example.kotlinserializer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSerializerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

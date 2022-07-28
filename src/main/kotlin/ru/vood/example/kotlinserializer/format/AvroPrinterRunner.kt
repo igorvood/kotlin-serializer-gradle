de1e92c2dac921a528e10a0aa58146b8e0c398a1
vood <balitsky_igor@list.ru>
2022-07-28 17:06:33 +0300

    s

--- src/main/kotlin/ru/vood/example/kotlinserializer/format/AvroPrinterRunner.kt
+++ src/main/kotlin/ru/vood/example/kotlinserializer/format/AvroPrinterRunner.kt
@@ -1,18 +1,30 @@
 package ru.vood.example.kotlinserializer.format
 
 import com.github.avrokotlin.avro4k.Avro
+import com.github.avrokotlin.avro4k.io.AvroBinaryOutputStream
+import com.github.avrokotlin.avro4k.io.AvroOutputStream
 import org.apache.avro.Schema
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Service
+import java.io.File
 
 @Service
 class AvroPrinterRunner : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        val schemaUaspDto: Schema = Avro.default.schema(UaspDto.serializer())
-        val schemaUaspDtoCuttten: Schema = Avro.default.schema(UaspDtoCuttten.serializer())
+        val schemaUaspDto= Avro.default.schema(UaspDto.serializer())
+        val schemaUaspDtoCuttten= Avro.default.schema(UaspDtoCuttten.serializer())
         println(schemaUaspDtoCuttten.toString(true))
 
         println(schemaUaspDto.toString(true))
+
+    }
+
+
+    fun asdasd(){
+
+        val schema = Avro.default.schema(UaspDto.serializer())
+
+        val os = AvroOutputStream.binary(schema, UaspDto.serializer()).to(File("pizzas.avro"))
     }
 }
